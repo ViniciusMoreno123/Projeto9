4d5707a72446136f81b2a9e42e942aceb7ec476d
ViniciusMoreno123 <93357216+ViniciusMoreno123@users.noreply.github.com>
2021-11-02 16:32:47 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var numero, numero1, numero2;
+
+function setup() {
+  createCanvas(400,400);
+ 
+  caixa = createSprite(200,200,50,50);
+ 
+  background("black");
+}
+
+function draw(){
+  numero = random(0,255);
+  numero1 = random(0,255);
+  numero2 = random(0,255);
+  numero3 = random(0,255);
+  numero4 = random(0,255);
+  numero5 = random(0,255);
+  
+if(keyDown(RIGHT_ARROW)){
+  background(numero, numero1, numero2); 
+}
+if(keyDown(LEFT_ARROW)){
+  background(numero3, numero4, numero5); 
+}
+drawSprites();
+}
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
